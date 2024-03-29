3353bb2f169976de37edcce52eea3523a7f2e7f4
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-29 11:28:12 +0300

    initial commit

---
+++ src/test/java/com/example/examcoursework/ExamCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examcoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamCourseWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
