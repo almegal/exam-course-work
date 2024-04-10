e6ceac856ee5c2b4c20bccab58209ec4e3ad3328
alekseipeikhel <alekseipeikhel@gmail.com>
2024-04-10 04:10:29 +0300

    добавил генерацию вопросов  по джаве и математике

--- src/main/java/com/example/examcoursework/service/examiner/implement/ExaminerServiceImpl.java
+++ src/main/java/com/example/examcoursework/service/examiner/implement/ExaminerServiceImpl.java
@@ -7,35 +7,37 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
-import java.util.Random;
 import java.util.Set;
 
 @Service
 public class ExaminerServiceImpl implements ExaminerService {
-    private final QuestionService questionService;
+    private final QuestionService javaQuestionService;
+    private final QuestionService mathQuestionService;
 
-    public ExaminerServiceImpl(@Qualifier("JavaQuestionServiceImpl") QuestionService questionService) {
-        this.questionService = questionService;
+    public ExaminerServiceImpl(
+            @Qualifier("JavaQuestionServiceImpl") QuestionService javaQuestionService,
+            @Qualifier("MathQuestionServiceImpl") QuestionService mathQuestionService) {
+        this.javaQuestionService = javaQuestionService;
+        this.mathQuestionService = mathQuestionService;
     }
     @Override
     public Set<Question> getQuestions(int amount) {
-        final Set<Question> questions = questionService.getAll();
+        final Set<Question> questions = javaQuestionService.getAll();
         // получаем размер списка и проверяем что есть необходимое количество вопросов в списке
         final int size = questions.size();
         if(size < amount || size == 0) throw new IllegalArgumentException("Количество необходимых вопросов превышает размер списка или равно 0");
-        // если количетсво необходимых вопросов равно размеру списка
-        // вернуть сразу весь список
-        if(size == amount) return questions;
         // создаем множество куда добавим уникальные вопросы и ответы
         Set<Question> result = new HashSet<>();
         // пока больше нуля
         while (amount > 0) {
             // получаем рандомные вопрос
-            Question queston = questionService.getRandomQuestion();
+            Question javaQuestion = javaQuestionService.getRandomQuestion();
+            Question mathQuestion = mathQuestionService.getRandomQuestion();
             // проверяем что вопрос добавленный уникален
-            boolean isAdded = result.add(queston);
+            boolean javaIsAdded = result.add(javaQuestion);
+            boolean mathIsAdded = result.add(mathQuestion);
             // если истинно уменьшаем значение
-            if(isAdded) amount--;
+            if(javaIsAdded && mathIsAdded) amount--;
         }
         return result;
     }
