8e26eddd60793d4504e3a4a43387ecad9abc0772
alekseipeikhel <alekseipeikhel@gmail.com>
2024-04-10 03:02:37 +0300

    контроллер

--- src/main/java/com/example/examcoursework/controller/ExaminerController.java
+++ src/main/java/com/example/examcoursework/controller/ExaminerController.java
@@ -1,7 +1,22 @@
 package com.example.examcoursework.controller;
 
+import com.example.examcoursework.model.Question;
+import com.example.examcoursework.service.examiner.ExaminerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Set;
+
+@RestController
+@RequestMapping
 public class ExaminerController {
+    private final ExaminerService examinerService;
+
+    public ExaminerController(ExaminerService examinerService) {
+        this.examinerService = examinerService;
+    }
+    @GetMapping(path="/get/{amount}")
+    public Set<Question> get(@PathVariable("amount") int amount) {
+        return examinerService.getQuestions(amount);
+    }
 }
