f67dfba3ce1cfa4fdb3182ea9d5752f56d516f69
alekseipeikhel <alekseipeikhel@gmail.com>
2024-04-10 03:03:15 +0300

    добавления статуса ответа ошибкам

--- src/main/java/com/example/examcoursework/Exception/ExceptionQuestionIsNotExsist.java
+++ src/main/java/com/example/examcoursework/Exception/ExceptionQuestionIsNotExsist.java
@@ -1,5 +1,9 @@
 package com.example.examcoursework.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class ExceptionQuestionIsNotExsist extends RuntimeException {
     public ExceptionQuestionIsNotExsist(String message) {
         super(message);
