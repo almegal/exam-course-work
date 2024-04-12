a34d2f7e7681d184e4649e9ad4d7e481d6a09886
alekseipeikhel <alekseipeikhel@gmail.com>
2024-04-12 08:35:10 +0300

    изменил поля сервисов на список сервисов переработана проверка на допустимые значения при получение вопроса и добалвения рандомного вопрсоа

--- src/main/java/com/example/examcoursework/service/examiner/implement/ExaminerServiceImpl.java
+++ src/main/java/com/example/examcoursework/service/examiner/implement/ExaminerServiceImpl.java
@@ -3,42 +3,42 @@ package com.example.examcoursework.service.examiner.implement;
 import com.example.examcoursework.model.Question;
 import com.example.examcoursework.service.examiner.ExaminerService;
 import com.example.examcoursework.service.question.QuestionService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class ExaminerServiceImpl implements ExaminerService {
-    private final QuestionService javaQuestionService;
-    private final QuestionService mathQuestionService;
+    private final List<QuestionService> questionServices;
 
-    public ExaminerServiceImpl(
-            @Qualifier("JavaQuestionServiceImpl") QuestionService javaQuestionService,
-            @Qualifier("MathQuestionServiceImpl") QuestionService mathQuestionService) {
-        this.javaQuestionService = javaQuestionService;
-        this.mathQuestionService = mathQuestionService;
+    public ExaminerServiceImpl(List<QuestionService> questionServices) {
+        this.questionServices = questionServices;
     }
+
     @Override
     public Set<Question> getQuestions(int amount) {
-        final Set<Question> javaQuestions = javaQuestionService.getAll();
-        final Set<Question> mathQuestions = javaQuestionService.getAll();
-        // получаем размер списка и проверяем что есть необходимое количество вопросов в списке
-        final int javaSize = javaQuestions.size();
-        final int mathSize = mathQuestions.size();
-        if((javaSize < amount || javaSize == 0) || (mathSize < amount || mathSize == 0)) {
+        // получаем общее количетсво вопросов
+        int size = 0;
+        for (QuestionService questionService : questionServices) {
+            size = size + questionService.getAll().size();
+        }
+        // если общее количество вопросов меньше запрашиваемого
+        // выбросить исключение
+        if (size < amount || size == 0) {
             throw new IllegalArgumentException("Количество необходимых вопросов превышает размер списка или равно 0");
         }
         // создаем множество куда добавим уникальные вопросы и ответы
         Set<Question> result = new HashSet<>();
-        // пока больше нуля
+        //  пока больше нуля
         while (result.size() < amount) {
-            // получаем рандомные вопрос
-            Question javaQuestion = javaQuestionService.getRandomQuestion();
-            Question mathQuestion = mathQuestionService.getRandomQuestion();
-            result.add(javaQuestion);
-            result.add(mathQuestion);
+            // получаемый рандомный индекс сервиса
+            int index = (int) Math.round(Math.random());
+            // получаем рандомный вопрос
+            // и добавляем в ответ
+            Question question = questionServices.get(index).getRandomQuestion();
+            result.add(question);
         }
         return result;
     }
